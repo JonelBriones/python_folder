6f63b9babb1da59f222f25c864086c12912e1794
Jonel Briones <jonel.c.briones@gmail.com>
2021-11-03 01:14:29 -0700

    Chaining Methods

--- fundamentals/OOP/Users.py
+++ fundamentals/OOP/Users.py
@@ -8,12 +8,14 @@ class User:		# here's what we have so far
     def make_deposit(self, amount):  # takes an argument that is the amount of the deposit
         # the specific user's account increases by the amount of the value received
         self.account_balance += amount
+        return self
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
-        return (f"User: {self.name}, Balance: ${self.account_balance}")
+        return(f"User: {self.name}, Balance: ${self.account_balance}")
 
     def transfer_money(self, other_user, account_balance):
         self.account_balance -= account_balance
@@ -25,18 +27,18 @@ class User:		# here's what we have so far
 jonel = User("Jonel Briones", "ijonel906@gmail.com")
 john = User("John-John Briones", "john-john@gmail.com")
 jonathan = User("Jonathan Briones", "jonathan@gmail.com")
-jonel.make_deposit(50)
-jonel.make_deposit(100)
-jonel.make_deposit(75)
-jonel.make_withdrawal(200)
+
+jonel.make_deposit(50).make_deposit(100).make_deposit(
+    75).make_withdrawal(200).display_user_balance()
 print(jonel.display_user_balance())
-john.make_deposit(100)
-john.make_deposit(200)
-john.make_withdrawal(300)
+
+john.make_deposit(101).make_deposit(
+    200).make_withdrawal(300).display_user_balance()
 print(john.display_user_balance())
-jonathan.make_deposit(1000)
-jonathan.make_withdrawal(200)
-jonathan.make_withdrawal(400)
-jonathan.make_withdrawal(50)
+
+jonathan.make_deposit(1000).make_withdrawal(
+    200).make_withdrawal(400).make_withdrawal(50).display_user_balance()
 print(jonathan.display_user_balance())
+
+
 jonel.transfer_money(jonathan, 5)
