79c45cdb6c14136ef922d7b54cabe37daa974983
Jonel Briones <jonel.c.briones@gmail.com>
2021-11-04 01:14:43 -0700

    Combined Users with Bank Account

--- fundamentals/OOP/BankAccount.py
+++ fundamentals/OOP/BankAccount.py
@@ -1,27 +1,40 @@
 class BankAccount:
+    all_accounts = []
 
     def __init__(self, int_rate, balance):
         self.int_rate = int_rate
         self.balance = balance
         balance = 0
+        BankAccount.all_accounts.append(self)
 
     def deposit(self, amount):
         self.balance += amount
         return self
 
     def withdraw(self, amount):
-        self.balance -= amount
+        if self.balance < amount:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+        else:
+            self.balance -= amount
         return self
 
     def display_account_info(self):
         print(f"${self.balance}")
 
     def yield_interest(self):
-        self.balance += self.int_rate * self.balance
+        if self.balance > 0:
+            self.balance += self.int_rate * self.balance
         return self
 
+    @classmethod
+    def all_banks_info(cls):
+        print("Printing All Bank Account Info")
+        for account in cls.all_accounts:
+            account.display_account_info()
 
-jonel = BankAccount(0.01, 100)
+
+jonel = BankAccount(0.01, -400)
 jonathan = BankAccount(0.01, 500)
 
 jonel.deposit(100).deposit(100).deposit(100).withdraw(
@@ -29,3 +42,5 @@ jonel.deposit(100).deposit(100).deposit(100).withdraw(
 
 jonathan.deposit(350).deposit(350).withdraw(25).withdraw(25).withdraw(
     50).withdraw(100).yield_interest().display_account_info()
+
+BankAccount .all_banks_info()
--- fundamentals/OOP/Users.py
+++ fundamentals/OOP/Users.py
@@ -1,25 +1,65 @@
+class BankAccount:
+    all_accounts = []
+
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+        balance = 0
+        BankAccount.all_accounts.append(self)
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if self.balance < amount:
+            self.balance -= (5 + amount)
+            print("Insufficient funds: Charging a $5 fee")
+
+        else:
+            self.balance -= amount
+        return self
+
+    def display_account_info(self):
+        print(f"${self.balance}")
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += self.int_rate * self.balance
+        return self
+
+    @classmethod
+    def all_banks_info(cls):
+        print("Printing All Bank Account Info")
+        for account in cls.all_accounts:
+            account.display_account_info()
+# above line is BankAccount
+
+
 class User:		# here's what we have so far
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.account_balance = 0
+        self.account = BankAccount(int_rate=0.02, balance=0)
     # adding the deposit method
 
     def make_deposit(self, amount):  # takes an argument that is the amount of the deposit
         # the specific user's account increases by the amount of the value received
-        self.account_balance += amount
+        self.account.deposit(amount)
+        print(self.account.balance)
         return self
 
     def make_withdrawal(self, amount):
-        self.account_balance -= amount
+        self.account.withdraw(amount)
+        print(self.account.balance)
         return self
 
     def display_user_balance(self):
-        return(f"User: {self.name}, Balance: ${self.account_balance}")
+        return(f"User: {self.name}, Balance: ${self.account.balance}")
 
-    def transfer_money(self, other_user, account_balance):
-        self.account_balance -= account_balance
-        other_user.account_balance += account_balance
+    def transfer_money(self, other_user, amount):
+        self.account.balance -= amount
+        other_user.account.balance += amount
         print((self.display_user_balance()))
         print((other_user.display_user_balance()))
 
@@ -28,8 +68,8 @@ jonel = User("Jonel Briones", "ijonel906@gmail.com")
 john = User("John-John Briones", "john-john@gmail.com")
 jonathan = User("Jonathan Briones", "jonathan@gmail.com")
 
-jonel.make_deposit(50).make_deposit(100).make_deposit(
-    75).make_withdrawal(200).display_user_balance()
+jonel.make_deposit(500).make_deposit(100).make_deposit(
+    75).make_withdrawal(680).display_user_balance()
 print(jonel.display_user_balance())
 
 john.make_deposit(101).make_deposit(
